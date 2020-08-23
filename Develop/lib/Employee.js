02df9edf4afc3bb7dcdaeeca3db9eaa174d7a9b0
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-22 18:01:37 -0700

    created a class in Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    name = this.name;
+    id = this.id;
+    email = this.email;
+  }
+
+  getName() {}
+  getId() {}
+  getEmail() {}
+  getRole() {}
+}
+module.exports = Employee;
